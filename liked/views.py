447260e0d17b5ab45f68bad235ceffb810906db0
Andrew Klevcov <andrew.klevcov@gmail.com>
2021-11-09 18:09:26 +0300

    '...'

---
+++ liked/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("qwer")
+
+# Create your views here.
---
+++ messages/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Я в своем познании настолько преисполнился")
+
+# Create your views here.
